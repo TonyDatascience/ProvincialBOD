963b3a4f8b2d6c541b1f44e19282b2c68dc9305f
TonyDatascience <thanawatpop@gmail.com>
2024-02-15 15:49:55 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.markdown("# Main page123 🎈")
+st.sidebar.markdown("# Main page 456 🎈")
+
+st.title("Trat Provincial Health Data v.1")
+st.image('images/kk.jpeg', caption='Trat sea')
+st.write_stream(stream_data)
