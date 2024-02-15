5ad1af8431a52fa5bcc0875b68a00f7d2620b530
TonyDatascience <thanawatpop@gmail.com>
2024-02-15 13:25:37 +0700

    JPYNB integrate

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -1,16 +1,18 @@
 import streamlit as st
+
+from streamlit_jupyter import StreamlitPatcher, tqdm
+
+StreamlitPatcher().jupyter() 
 import pandas as pd
 import numpy as np
 
 # In[2]:
 
-
+st.title("Hello")
 st.write("""
 # Hello *world* 123
 """)
 
-
-# In[ ]:
 
 
 
