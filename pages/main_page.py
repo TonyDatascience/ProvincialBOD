f1e80a5fb7737b67ece94bd14a788e523970c4a6
TonyDatascience <thanawatpop@gmail.com>
2024-02-15 15:39:51 +0700

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
 
-if st.button("Stream data"):
-    st.write_stream(stream_data)
-
-
-st.markdown("# Main page 🎈")
+st.markdown("# Main page123 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
 st.title("Trat Provincial Health Data v.1")
 st.image('images/kk.jpeg', caption='Trat sea')
 st.write_stream(stream_data)
