472c84b7538d5d090f3f7cb65fdfe3135bace177
TonyDatascience <thanawatpop@gmail.com>
2024-02-15 15:55:10 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-st.markdown("# Main page123 🎈")
-st.sidebar.markdown("# Main page 456 🎈")
+st.markdown("# Main page123🎈")
+st.sidebar.markdown("# Main page123🎈")
 
 st.title("Trat Provincial Health Data v.1")
 st.image('images/kk.jpeg', caption='Trat sea')
