18743069ddd3b80017ccba96efaa95f062d1892f
TonyDatascience <thanawatpop@gmail.com>
2024-02-15 14:21:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,33 +4,4 @@ import pandas as pd
 import copy
 import matplotlib.pyplot as plt 
 
-
-"""
-import tensorflow as tf
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Input, Dropout, LSTM, Activation, Conv1D,MaxPooling1D,Flatten,GlobalAveragePooling1D,GlobalMaxPooling1D
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.initializers import glorot_uniform
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras import layers
-from keras.layers import Flatten
-from keras import backend as K
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
-from sklearn.metrics import mean_squared_error
-
-
-#import streamlit as st
-#from streamlit_jupyter import StreamlitPatcher, tqdm
-#StreamlitPatcher().jupyter() 
-
-#---------Block 2
-model_file_120_70='Models/cnn_100_70_Feb_13.keras'
-scalerfile_120_70='Models/cnn_100_70_scaler_Feb_13.sav'
-model_file_100_70='Models/cnn_120_70_Feb_14.keras'
-scalerfile_100_70='Models/cnn_120_70_scaler_Feb_14.sav'
-std_scaler_120_70=joblib.load(scalerfile_120_70)
-model_cnn3_120_70=tf.keras.models.load_model(model_file_120_70)
-std_scaler_100_70=joblib.load(scalerfile_100_70)
-model_cnn3_100_70=tf.keras.models.load_model(model_file_100_70)
-"""
+st.title("Hello")
